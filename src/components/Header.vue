a360e02c8e3aa6815b43c9e2c32c9a75863beb55
ToniDepp <124863328+ToniDepp@users.noreply.github.com>
2025-03-24 09:55:56 -0600

    Correciones Ortograficas

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header>
-      <div class="logo">TimShop</div>
+      <div class="logo">Horario</div>
       <div class="search-bar">
         <input type="text" placeholder="Buscar productos..." />
         <button>🔍</button>
