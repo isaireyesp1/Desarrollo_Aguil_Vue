7737f4839802580bcb456e70a60e4f8174bcadf6
ToniDepp <124863328+ToniDepp@users.noreply.github.com>
2025-03-29 19:23:24 -0600

    props

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,10 @@
 <script setup>
-import Header from './components/Header.vue'
-import ProductList from './components/ProductList.vue'
-import Footer from './components/Footer.vue'
+import Padre from './components/Props/Padre.vue'
 </script>
 
 <template>
-  <div class="main-container">
-    <Header />
-    <main>
-      <ProductList />
-    </main>
-    <Footer />
-  </div>
+<!--<Principal nombrecito="Eduardo Tomas" />-->
+<Padre/>
 </template>
-
 <style scoped>
-/* Reset general */
-* {
-  margin: 0;
-  padding: 0;
-  box-sizing: border-box;
-  font-family: 'Inter', sans-serif;
-}
-
-/* Fondo general */
-body {
-  background-color: #FFFFFF; /* Fondo blanco */
-  color: #000000; /* Texto negro */
-  line-height: 1.6;
-}
-
-/* Contenedor principal */
-.main-container {
-  display: flex;
-  flex-direction: column;
-  min-height: 100vh;
-}
-
-main {
-  flex-grow: 1;
-  padding: 20px;
-}
 </style>
---
+++ src/components/Principal.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+    nombrecito: String,
+})
+</script> 
+
+<template>
+    <h1>{{ nombrecito }}</h1>
+</template>
+
+<style scoped>
+</style>
